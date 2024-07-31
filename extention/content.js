d8516aac11689ba02612a597ca6791f350836a5a
kitashimauni <110659558+kitashimauni@users.noreply.github.com>
2024-07-31 15:42:33 +0900

    show time

--- extention/content.js
+++ extention/content.js
@@ -1,7 +1,9 @@
-const GETINFO_INTERVAL = 1000
+const GETINFO_INTERVAL = 500;
 
 const TITLE_CLASS_NAME = "pauseInfoTxt1";
 const EPISODES_CLASS_NAME = "pauseInfoTxt2";
+const TIME_CLASS_NAME = "time";
+const TIME_ID_NAME = "#time";
 const VIDEO_TAG_NAME = "video";
 
 function getInfo() {
@@ -12,24 +14,27 @@ function getInfo() {
     if (!videoElement) {
         console.log("Couldn't get playing status");
     } else {
-        data.playing = !Boolean(videoElement.paused)
+        data.playing = !Boolean(videoElement.paused);
     }
 
     // title and episodes
     const titleElement = document.getElementsByClassName(TITLE_CLASS_NAME)[0];
     const episodeElement = document.getElementsByClassName(EPISODES_CLASS_NAME)[0];
-    if (!titleElement || !episodeElement) {
-        console.log("Couldn't get title or eipsodes");
+    const timeElement = document.getElementsByClassName(TIME_CLASS_NAME)[0];
+    if (!titleElement || !episodeElement || !timeElement) {
+        console.log("Couldn't get title or eipsodes or time");
     } else {
         const title = titleElement.textContent;
         const episodes = episodeElement.textContent;
-        data.title = title + " " + episodes
+        const time = timeElement.querySelector(TIME_ID_NAME).textContent;
+        data.title = title + " " + episodes;
+        data.time = time;
     }
 
     data.connection = true;
 
     // return data
-    return data
+    return data;
 }
 
 // send message to background
@@ -43,5 +48,10 @@ setInterval(sendMessage, GETINFO_INTERVAL);
 
 // disconnect when close tab
 window.addEventListener("beforeunload", () => {
-    chrome.runtime.sendMessage({"playing": false, "title": "", "connection": false}, (response) => true);
-})
+    chrome.runtime.sendMessage({
+        "playing": false,
+        "title": "",
+        "time": "",
+        "connection": false
+    }, (response) => true)
+});
